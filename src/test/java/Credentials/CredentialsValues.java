95e97d9fb9fc2dfca67e9ac7621fb8378eee019a
Dmitriy Kulikov <dvkulikov@avito.ru>
2021-03-06 20:11:12 +0300

    Init

--- src/test/java/Credentials/CredentialsValues.java
+++ src/test/java/Credentials/CredentialsValues.java
@@ -1,9 +1,9 @@
 package Credentials;
 
 public interface CredentialsValues {
-    String ACCESS_TOKEN = "baffe176851b18fbdc6e82b1aa9de7ca51ef49cb0d3fb6880ec195e5cb75b91b748cb6ee5662e26473c6d";
-    Integer ACCOUNT_ID_NUMBER = 503923867;
-    String GROUP_ID = "-203027909";
+    String ACCESS_TOKEN = "";
+    Integer ACCOUNT_ID_NUMBER = 1234567;
+    String GROUP_ID = "";
     String TYPE_OF_DATA = "post";
     String API_VERSION = "5.130";
     Integer APP_ID = 1;
